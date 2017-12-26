ef71c1af38040fdff399ed675f7012f6878a460c
arjun <arjun.94.choudhry@gmail.com>
2017-12-26 07:54:54 -0600

    fitting the model and plotting the graph

--- Hierarchial_Clustering/heirarchial.py
+++ Hierarchial_Clustering/heirarchial.py
@@ -19,3 +19,20 @@ plt.ylabel('Euclidean Distance btw the centroids of the clusters')
 plt.show()
 # From the above dendogram, we can see that the optimal number of clusters is 5
 
+# Fitting the hierarchial to the datset
+from sklearn.cluster import AgglomerativeClustering
+hcCluster= AgglomerativeClustering(n_clusters = 5,affinity = 'euclidean', linkage = 'ward')
+y_hc = hcCluster.fit_predict(X)
+
+# plotting the graph
+plt.scatter(X[y_hc == 0,0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster1')
+plt.scatter(X[y_hc == 1,0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster2')
+plt.scatter(X[y_hc == 2,0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster3')
+plt.scatter(X[y_hc == 3,0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster4')
+plt.scatter(X[y_hc == 4,0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster5')
+
+plt.title('Clusters of clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
